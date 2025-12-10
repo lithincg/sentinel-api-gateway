e20de0a0e3461a0500cbca44bbae0413820a5723
Lithin C G <lithinraju1@gmail.com>
2025-12-10 22:07:47 +0530

    Added UserService skeleton, BCrypt config, and Spring Security

---
+++ src/main/java/com/sentinel/apigateway/service/UserService.java
@@ -0,0 +1,15 @@
+package com.sentinel.apigateway.service;
+
+import com.sentinel.apigateway.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+}
