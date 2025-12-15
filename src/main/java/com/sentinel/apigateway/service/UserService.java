3108bd1d957d71a6caf03db6c817def67828a27e
Lithin C G <lithinraju1@gmail.com>
2025-12-15 23:48:42 +0530

    Implemented secure ApiKey generation and added UserNotFoundException

--- src/main/java/com/sentinel/apigateway/service/UserService.java
+++ src/main/java/com/sentinel/apigateway/service/UserService.java
@@ -1,12 +1,18 @@
 package com.sentinel.apigateway.service;
 
+import com.sentinel.apigateway.entity.ApiKey;
 import com.sentinel.apigateway.entity.User;
 import com.sentinel.apigateway.exception.DuplicateUserException;
+import com.sentinel.apigateway.exception.UserNotFoundException;
+import com.sentinel.apigateway.repository.ApiKeyRepository;
 import com.sentinel.apigateway.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +20,7 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
+    private final ApiKeyRepository apiKeyRepository;
     @Transactional
     public User registerUser(String email, String rawPassword){
         if(userRepository.findByEmail(email).isPresent()){
@@ -26,6 +33,27 @@ public class UserService {
                 .build();
         userRepository.save(user);
         return user;
+    }
+    @Transactional
+    public String generateApiKey(String email){
+        Optional<User> user = userRepository.findByEmail(email);
+        if(user.isPresent()){
+            var clearKey = UUID.randomUUID().toString().replace("-", "");
+            var hashedKey = passwordEncoder.encode(clearKey);
+            var previewKey = clearKey.substring(0, 5);
+            ApiKey apiKey = ApiKey
+                                .builder()
+                                .keyHash(hashedKey)
+                                .keyPrefix(previewKey)
+                                .user(user.get())
+                                .build();
+            apiKeyRepository.save(apiKey);
+            return clearKey;
+        }
+
+
+        throw new UserNotFoundException("User not found");
+
     }
 
 }
