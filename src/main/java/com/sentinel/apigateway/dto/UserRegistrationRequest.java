b7f5dd7c3c2b1e1c5b51e5de12680a289058f3d8
Lithin C G <lithinraju1@gmail.com>
2025-12-16 16:44:51 +0530

    Add Validation constraints to UserRegistrationRequest DTO

--- src/main/java/com/sentinel/apigateway/dto/UserRegistrationRequest.java
+++ src/main/java/com/sentinel/apigateway/dto/UserRegistrationRequest.java
@@ -1,3 +1,15 @@
 package com.sentinel.apigateway.dto;
 
-public record UserRegistrationRequest(String email, String password) {}
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UserRegistrationRequest(
+            @NotBlank(message="email required")
+            @Email
+            String email,
+            @NotBlank(message="Password must not be blank")
+            @Size(min=8,message = "Password must have atleast 8 characters")
+            String password) {
+
+}
