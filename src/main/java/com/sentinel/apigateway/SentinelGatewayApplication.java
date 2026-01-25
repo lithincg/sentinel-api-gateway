5a1fab9b44b00d7f7e85c4618b7549dc85c312e1
Lithin C G <lithinraju1@gmail.com>
2026-01-25 22:13:57 +0530

    fix: force JVM timezone to UTC to resolve Postgres connection error

--- src/main/java/com/sentinel/apigateway/SentinelGatewayApplication.java
+++ src/main/java/com/sentinel/apigateway/SentinelGatewayApplication.java
@@ -2,9 +2,14 @@ package com.sentinel.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class SentinelGatewayApplication {
+
+    static {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SentinelGatewayApplication.class, args);
