ab6a222b1af00126a74fb3d32f69ee556c6fbf32
Lithin C G <lithinraju1@gmail.com>
2026-02-16 22:54:03 +0530

    update with new interface and to use atomic operations

--- src/main/java/com/sentinel/apigateway/repository/InMemoryRateLimitRepository.java
+++ src/main/java/com/sentinel/apigateway/repository/InMemoryRateLimitRepository.java
@@ -1,24 +1,64 @@
 package com.sentinel.apigateway.repository;
 
 import org.springframework.stereotype.Repository;
-
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
-public class InMemoryRateLimitRepository implements  RateLimitRepository {
-    private final ConcurrentHashMap<String, AtomicInteger> counters = new ConcurrentHashMap<>();
+public class InMemoryRateLimitRepository implements RateLimitRepository {
+
+    private final Map<String, AtomicLong> windowStartTimes = new ConcurrentHashMap<>();
+    private final Map<String, AtomicInteger> requestCounts = new ConcurrentHashMap<>();
 
     @Override
     public long recordRequest(String apiKey, long windowMs, int maxRequests) {
-        //temporary Fixed window logic. dont forget to change later.
-        AtomicInteger counter = counters.computeIfAbsent(apiKey, k -> new AtomicInteger(0));
-        return counter.incrementAndGet();
+        long now = System.currentTimeMillis();
+
+        AtomicLong windowStart = windowStartTimes.computeIfAbsent(
+                apiKey,
+                k -> new AtomicLong(now)
+        );
+        AtomicInteger count = requestCounts.computeIfAbsent(
+                apiKey,
+                k -> new AtomicInteger(0)
+        );
+
+        long currentWindowStart = windowStart.get();
+        if (now - currentWindowStart >= windowMs) {
+            windowStart.set(now);
+            count.set(0);
+        }
+
+        int currentCount = count.get();
+        if (currentCount < maxRequests) {
+            int newCount = count.incrementAndGet();
+
+            if (newCount <= maxRequests) {
+                return 1;
+            } else {
+                count.decrementAndGet();
+                return 0;
+            }
+        } else {
+            return 0;
+        }
     }
 
     @Override
     public long getCurrentCount(String apiKey, long windowMs) {
-        AtomicInteger counter = counters.get(apiKey);
-        return (counter != null) ? counter.get() : 0;
+        AtomicLong windowStart = windowStartTimes.get(apiKey);
+        if (windowStart == null) {
+            return 0;
+        }
+
+        long now = System.currentTimeMillis();
+        if (now - windowStart.get() >= windowMs) {
+            return 0;
+        }
+
+        AtomicInteger count = requestCounts.get(apiKey);
+        return count != null ? count.get() : 0;
     }
 }
