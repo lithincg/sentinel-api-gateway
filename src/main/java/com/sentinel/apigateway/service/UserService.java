78435881f554ea0338e55ae94a9e369256c0d292
Lithin C G <lithinraju1@gmail.com>
2026-02-20 01:54:25 +0530

    inject PasswordEncoder interface in UserService to resolve bean type mismatch

--- src/main/java/com/sentinel/apigateway/service/UserService.java
+++ src/main/java/com/sentinel/apigateway/service/UserService.java
@@ -8,7 +8,7 @@ import com.sentinel.apigateway.exception.UserNotFoundException;
 import com.sentinel.apigateway.repository.ApiKeyRepository;
 import com.sentinel.apigateway.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,7 +20,7 @@ import java.util.UUID;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
     private final ApiKeyRepository apiKeyRepository;
     @Transactional
     public User registerUser(UserRegistrationRequest userRegistrationRequest) {
