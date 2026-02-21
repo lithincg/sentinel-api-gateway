3c5eb161f2a844e8634be86d7ef093fc03c49250
Lithin C G <lithinraju1@gmail.com>
2026-02-21 19:12:25 +0530

    remove unused imports across ApiKey, RedisConfig, GlobalExceptionHandler.

--- src/main/java/com/sentinel/apigateway/entity/ApiKey.java
+++ src/main/java/com/sentinel/apigateway/entity/ApiKey.java
@@ -3,7 +3,6 @@ package com.sentinel.apigateway.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
