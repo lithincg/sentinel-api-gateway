3a05ff36c08f1b4265b4371de8d0d93b2a3f267b
Lithin C G <lithinraju1@gmail.com>
2026-02-23 10:27:33 +0530

    add POST /api/auth/login endpoint to AuthController

--- src/main/java/com/sentinel/apigateway/controller/AuthController.java
+++ src/main/java/com/sentinel/apigateway/controller/AuthController.java
@@ -1,8 +1,11 @@
 package com.sentinel.apigateway.controller;
 
+import com.sentinel.apigateway.dto.LoginRequest;
+import com.sentinel.apigateway.dto.LoginResponse;
 import com.sentinel.apigateway.dto.UserRegistrationRequest;
 import com.sentinel.apigateway.dto.UserRegistrationResponse;
 import com.sentinel.apigateway.entity.User;
+import com.sentinel.apigateway.service.JwtUtil;
 import com.sentinel.apigateway.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final UserService userService;
+    private final JwtUtil jwtUtil;
 
     @PostMapping("/register")
     public ResponseEntity<UserRegistrationResponse> register(@Valid @RequestBody UserRegistrationRequest request) {
@@ -23,5 +27,11 @@ public class AuthController {
         UserRegistrationResponse response = new UserRegistrationResponse(registeredUser.getId(),
                                             registeredUser.getEmail(),registeredUser.getRole().name());
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest request) {
+    LoginResponse loginResponse= new LoginResponse(userService.login(request.email(), request.password()));
+    return ResponseEntity.ok(loginResponse);
     }
 }
