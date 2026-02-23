f28b0435fa2dd3a732d760f9d7e8716f1fb3ac5c
Lithin C G <lithinraju1@gmail.com>
2026-02-23 10:26:48 +0530

    add LoginRequest and LoginResponse DTOs

---
+++ src/main/java/com/sentinel/apigateway/dto/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.sentinel.apigateway.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record LoginRequest(@NotBlank(message="email required")
+                           @Email
+                           String email,
+                           @NotBlank(message="Password must not be blank")
+                           @Size(min=8,message = "Password must have atleast 8 characters")
+                           String password){
+
+}
+
