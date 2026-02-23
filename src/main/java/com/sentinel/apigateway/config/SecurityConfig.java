da52c2eca19631c5c78125f895fb5629ef8100ad
Lithin C G <lithinraju1@gmail.com>
2026-02-23 20:38:07 +0530

    update SecurityConfig to add JwtAuthFilter and stateless session management

--- src/main/java/com/sentinel/apigateway/config/SecurityConfig.java
+++ src/main/java/com/sentinel/apigateway/config/SecurityConfig.java
@@ -2,12 +2,14 @@ package com.sentinel.apigateway.config;
 
 import com.sentinel.apigateway.filter.RateLimiterFilter;
 import com.sentinel.apigateway.security.ApiKeyAuthFilter;
+import com.sentinel.apigateway.security.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,16 +19,22 @@ public class SecurityConfig {
 
     private final ApiKeyAuthFilter apiKeyAuthFilter;
     private final RateLimiterFilter  rateLimiterFilter;
+    private final JwtAuthFilter jwtAuthFilter;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/health", "/api/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .addFilterBefore(rateLimiterFilter, org.springframework.security.web.header.HeaderWriterFilter.class)
                 .addFilterBefore(apiKeyAuthFilter, org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(jwtAuthFilter, ApiKeyAuthFilter.class)
+                .addFilterBefore(rateLimiterFilter, org.springframework.security.web.header.HeaderWriterFilter.class)
                 .build();
     }
 }
