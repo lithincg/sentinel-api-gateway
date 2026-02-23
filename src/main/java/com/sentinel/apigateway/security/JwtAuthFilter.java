b014ee49f43c089fe1ba1e98a3ce99717ac1eb45
Lithin C G <lithinraju1@gmail.com>
2026-02-23 20:37:13 +0530

    add JwtAuthFilter for JWT validation and authentication

---
+++ src/main/java/com/sentinel/apigateway/security/JwtAuthFilter.java
@@ -0,0 +1,63 @@
+package com.sentinel.apigateway.security;
+
+
+import com.sentinel.apigateway.service.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request){
+        String path = request.getRequestURI();
+        if(path.startsWith("/health") || path.startsWith("/api/auth/"))
+            return true;
+        else{
+            String apiKeyHeader = request.getHeader("X-API-KEY");
+            if(apiKeyHeader != null)
+                return true;
+            else
+                return false;
+        }
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String jwt = authHeader.substring(7);
+            if (jwtUtil.validateToken(jwt)) {
+                var auth = new UsernamePasswordAuthenticationToken(
+                        jwtUtil.getEmail(jwt),
+                        null,
+                        Collections.emptyList()
+                );
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            } else {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+}
