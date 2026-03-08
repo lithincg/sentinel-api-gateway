bc586772ddf50cd985d1090f7f1389499e6b6ed3
Lithin C G <lithinraju1@gmail.com>
2026-03-08 05:45:56 +0530

    setting to original limit

--- src/main/java/com/sentinel/apigateway/service/RateLimiterService.java
+++ src/main/java/com/sentinel/apigateway/service/RateLimiterService.java
@@ -4,9 +4,6 @@ import com.sentinel.apigateway.repository.RateLimitRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
-
 @Service
 @RequiredArgsConstructor
 public class RateLimiterService {
@@ -16,7 +13,7 @@ public class RateLimiterService {
     public boolean allowRequest(String apiKey) {
 
         long windowMs = 60000;
-        int maxRequests = 1000000;
+        int maxRequests = 100;
 
 
         long result = rateLimitRepository.recordRequest(apiKey, windowMs, maxRequests);
